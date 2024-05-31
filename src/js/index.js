d51d562561bc52b1d4126bedb8785e0adc325eff
Gabriele <gabgri14@gmail.com>
2024-05-31 16:09:52 +0200

    old code removed

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,6 @@ import "../scss/style.scss";
 import { getNews, getInfo } from "./api";
 
 let flag = 0;
-
 async function loadTenNews() {
   let listId = await getNews();
   let tenId = listId.slice(flag, flag+10);
@@ -25,24 +24,6 @@ function insertArticle(obj) {
   article.appendChild(myArticle);
 }
 
-
-/*
-// HackerNews API - All id 
-async function getNewsId() {
-    const request = 'https://hacker-news.firebaseio.com/v0/newstories.json';
-    const response = await fetch(request);
-    const id = await response.json();
-    return id;
-}
-
-// HackerNews API - info 
-async function getInfoId(id) {
-    const request = `https://hacker-news.firebaseio.com/v0/item/${id}.json`
-    const response = await fetch(request);
-    const infoId = await response.json();
-    return infoId;
-}
-*/
 const btn = document.querySelector(".load-more");
 btn.addEventListener("click", loadTenNews);
 
