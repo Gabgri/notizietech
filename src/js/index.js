290425352d78d93f6ceb398f8e36e263cdeebd1f
Gabriele <gabgri14@gmail.com>
2024-06-01 15:59:36 +0200

    function formattedTime

--- src/js/index.js
+++ src/js/index.js
@@ -1,28 +1,48 @@
 import "../scss/style.scss";
 import { getNews, getInfo } from "./api";
 
+function formattedTime(timestamp) {
+  let date = new Date(timestamp * 1000).toLocaleString();
+  return date
+}
+
 let flag = 0;
 async function loadTenNews() {
+  let tenArticles = [];
   let listId = await getNews();
   let tenId = listId.slice(flag, flag+10);
   for (let i = 0; i < tenId.length; i++) {
     let infoId = await getInfo(tenId[i]);
+    let articleDate = formattedTime(infoId.time);
     let article = {
       title: infoId.title,
       link: infoId.url,
-      time: infoId.time,
+      time: articleDate,
     }
-    insertArticle(article);
+    tenArticles.push(article);
+    //insertArticle(article);
     flag += 1;
+  }
+  insertTenArticles(tenArticles);
+}
+
+function insertTenArticles(articles) {
+  for (let i = 0; i < articles.length; i++) {
+    const article = document.getElementById('news');
+    const myArticle = document.createElement('article');
+    myArticle.innerHTML = `<h3>${articles[i].title}</h3><a href="${articles[i].link}" target="_blank">Open the article</a><p>${articles[i].time}</p>`;
+    article.appendChild(myArticle);
   }
 }
 
+/*
 function insertArticle(obj) { 
   const article = document.getElementById('news');
   const myArticle = document.createElement('article');
-  myArticle.innerHTML = `<h3>${obj.title}</h3><a href="${obj.link}" target="_blank">${obj.link}</a><p>${obj.time}</p>`;
+  myArticle.innerHTML = `<h3>${obj.title}</h3><a href="${obj.link}" target="_blank">Open the article</a><p>${obj.time}</p>`;
   article.appendChild(myArticle);
 }
+*/
 
 const btn = document.getElementById("load-more");
 btn.addEventListener("click", loadTenNews);
